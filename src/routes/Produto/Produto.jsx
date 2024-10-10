fe0c3e1ee964d1f0b599ba4fd80643760a1663a1
Laboratório FIAP <brunobiletsky@gmail.com>
2024-10-10 08:55:16 -0300

    criando servidor json

--- src/routes/Produto/Produto.jsx
+++ src/routes/Produto/Produto.jsx
@@ -1,7 +1,11 @@
+import { useState, useEffect } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+
 const Produto =()=>{
     return(
         <>
         <h1>Cadastrar Produto</h1>
+        
         </>
     )
 }
